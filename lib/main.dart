e47d6aa50a67b6dd7b2acc1fdc96bc1e3d414d03
Shriya Thakur <102856369+shriya4320@users.noreply.github.com>
2024-05-12 16:04:58 +0530

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+import 'Presentation/Routes/generated_routes.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter swipable animations',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: '/',
+      onGenerateRoute: RouteGenerator().generateRoute,
+    );
+  }
+}
